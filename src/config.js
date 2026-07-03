4b3f6bba042496ad0efdd6a703f64d38840e9af8
vijayakumar-kumaran <vijayakumark6186@gmail.com>
2026-07-03 15:30:35 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // Central configuration for frontend
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://easy-audit-backend-nnit.onrender.com';
 
 export default API_BASE_URL;
